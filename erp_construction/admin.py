797c5f9aaefdcf80fb3a515537f99f0890818807
extraordinaire <peter@chenchatech.com>
2019-05-06 10:46:06 +0300

    Added temporary user model to take care of relationship with the newly added project model.

--- erp_construction/admin.py
+++ erp_construction/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Project
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'project_name', 'location', 'created_by', 'created_at', 'updated_at', 'is_active')
+    list_display_links = ('project_name', )
+    search_fields = ('project_name', )
+    list_editable = ('is_active',)
+
+
+admin.site.register(Project, ProjectAdmin)
--- erp_construction/models.py
+++ erp_construction/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    employee_id = models.IntegerField()
+    password = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.employee_id)
+
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=100, unique=True)
+    location = models.CharField(max_length=200)
+    created_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.project_name
