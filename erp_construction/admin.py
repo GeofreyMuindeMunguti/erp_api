32abf8b488541e34d5341bb68ae7b57c23cd4c17
extraordinaire <peter@chenchatech.com>
2019-05-06 14:26:06 +0300

    Models for project, commercial, procurement, civil and installation teams. Including images, health documents, access approvals and a temporary user model.

--- erp_construction/admin.py
+++ erp_construction/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
-from .models import User, Project
+from .models import User, Project, ProcurementTeam, HealthDocumentsCivilTeam, CivilWorksTeam, FoundationImage, BTSAndGeneatorSlabsImage, SiteWallingImage, RFAndLinkImage, ElectricalImage, KPLCSolarImage, CommercialTeam, AccessApprovalCivil, AccessApprovalInstallation, HealthDocumentsInstallationTeam, InstallationTeam
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'employee_id', 'password', 'created_at', 'updated_at', 'is_active')
+    list_display_links = ('employee_id', )
+    search_fields = ('employee_id', )
+    list_editable = ('is_active',)
+
+
+admin.site.register(User, UserAdmin)
 
 
 class ProjectAdmin(admin.ModelAdmin):
@@ -11,3 +21,155 @@ class ProjectAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Project, ProjectAdmin)
+
+
+class AccessApprovalCivilAdmin(admin.ModelAdmin):
+    list_display = ('id', 'project_name', 'access_approval', 'created_at', 'updated_at', 'is_active')
+    list_display_links = ('project_name', )
+    search_fields = ('project_name', )
+    list_editable = ('is_active',)
+
+
+admin.site.register(AccessApprovalCivil, AccessApprovalCivilAdmin)
+
+
+class AccessApprovalInstallationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'project_name', 'access_approval', 'created_at', 'updated_at', 'is_active')
+    list_display_links = ('project_name', )
+    search_fields = ('project_name', )
+    list_editable = ('is_active',)
+
+
+admin.site.register(AccessApprovalInstallation, AccessApprovalInstallationAdmin)
+
+
+class CommercialTeamAdmin(admin.ModelAdmin):
+    list_display = ('id', 'project_name', 'po_file', 'po_file_comment', 'initial_invoice', 'initial_invoice_comment', 'posted_by', 'is_approved', 'created_at', 'updated_at', 'is_active')
+    list_display_links = ('project_name', )
+    list_filter = ('project_name',)
+    search_fields = ('project_name', )
+    list_editable = ('is_active',)
+
+
+admin.site.register(CommercialTeam, CommercialTeamAdmin)
+
+
+class ProcurementTeamAdmin(admin.ModelAdmin):
+    list_display = ('id', 'project_name', 'po_steel', 'po_steel_comment', 'po_electrical_materials', 'po_electrical_materials_comment', 'po_subcontractors', 'po_subcontractors_comment', 'posted_by', 'is_approved', 'created_at', 'updated_at', 'is_active')
+    list_display_links = ('project_name', )
+    list_filter = ('project_name',)
+    search_fields = ('project_name', )
+    list_editable = ('is_active',)
+
+
+admin.site.register(ProcurementTeam, ProcurementTeamAdmin)
+
+
+class HealthDocumentsCivilTeamAdmin(admin.ModelAdmin):
+    list_display = ('id', 'project_name', 'job_hazard_form', 'job_hazard_form_comment', 'incident_notification_form', 'incident_notification_form_comment', 'toolbox_meeting_form', 'toolbox_meeting_form_comment', 'communication_plan_form', 'communication_plan_form_comment', 'posted_by', 'is_approved', 'created_at', 'updated_at', 'is_active')
+    list_display_links = ('project_name', )
+    list_filter = ('project_name',)
+    search_fields = ('project_name', )
+    list_editable = ('is_active',)
+
+
+admin.site.register(HealthDocumentsCivilTeam, HealthDocumentsCivilTeamAdmin)
+
+
+class FoundationImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'project_name', 'foundation_and_curing_image', 'created_at', 'updated_at', 'is_active')
+    list_display_links = ('project_name', )
+    list_filter = ('project_name',)
+    search_fields = ('project_name', )
+    list_editable = ('is_active',)
+
+
+admin.site.register(FoundationImage, FoundationImageAdmin)
+
+
+class BTSAndGeneatorSlabsImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'project_name', 'bts_and_generator_slabs_image', 'created_at', 'updated_at', 'is_active')
+    list_display_links = ('project_name', )
+    list_filter = ('project_name',)
+    search_fields = ('project_name', )
+    list_editable = ('is_active',)
+
+
+admin.site.register(BTSAndGeneatorSlabsImage, BTSAndGeneatorSlabsImageAdmin)
+
+
+class SiteWallingImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'project_name', 'site_walling_image', 'created_at', 'updated_at', 'is_active')
+    list_display_links = ('project_name', )
+    list_filter = ('project_name',)
+    search_fields = ('project_name', )
+    list_editable = ('is_active',)
+
+
+admin.site.register(SiteWallingImage, SiteWallingImageAdmin)
+
+
+class RFAndLinkImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'project_name', 'rf_and_link_installation_image', 'created_at', 'updated_at', 'is_active')
+    list_display_links = ('project_name', )
+    list_filter = ('project_name',)
+    search_fields = ('project_name', )
+    list_editable = ('is_active',)
+
+
+admin.site.register(RFAndLinkImage, RFAndLinkImageAdmin)
+
+
+class ElectricalImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'project_name', 'electrical_installation_image', 'created_at', 'updated_at', 'is_active')
+    list_display_links = ('project_name', )
+    list_filter = ('project_name',)
+    search_fields = ('project_name', )
+    list_editable = ('is_active',)
+
+
+admin.site.register(ElectricalImage, ElectricalImageAdmin)
+
+
+class KPLCSolarImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'project_name', 'kplc_solar_installation_image', 'created_at', 'updated_at', 'is_active')
+    list_display_links = ('project_name', )
+    list_filter = ('project_name',)
+    search_fields = ('project_name', )
+    list_editable = ('is_active',)
+
+
+admin.site.register(KPLCSolarImage, KPLCSolarImageAdmin)
+
+
+class CivilWorksTeamAdmin(admin.ModelAdmin):
+    list_display = ('id', 'project_name', 'health_documents_civil', 'health_documents_comment', 'access_approvals', 'access_approval_comment', 'foundation_images', 'foundation_and_curing_comment', 'slabs_images', 'bts_and_generator_slabs_comment', 'site_walling_images', 'site_walling_images_comment', 'posted_by', 'is_approved', 'created_at', 'updated_at', 'is_active')
+    list_display_links = ('project_name', )
+    list_filter = ('project_name',)
+    search_fields = ('project_name', )
+    list_editable = ('is_active',)
+
+
+admin.site.register(CivilWorksTeam, CivilWorksTeamAdmin)
+
+
+class HealthDocumentsInstallationTeamAdmin(admin.ModelAdmin):
+    list_display = ('id', 'project_name', 'job_hazard_form', 'job_hazard_form_comment', 'incident_notification_form', 'incident_notification_form_comment', 'toolbox_meeting_form', 'toolbox_meeting_form_comment', 'communication_plan_form', 'communication_plan_form_comment', 'posted_by', 'is_approved', 'created_at', 'updated_at', 'is_active')
+    list_display_links = ('project_name', )
+    list_filter = ('project_name',)
+    search_fields = ('project_name', )
+    list_editable = ('is_active',)
+
+
+admin.site.register(HealthDocumentsInstallationTeam, HealthDocumentsInstallationTeamAdmin)
+
+
+class InstallationTeamAdmin(admin.ModelAdmin):
+    list_display = ('id', 'project_name', 'health_documents_installation', 'health_documents_comment', 'access_approvals', 'access_approval_comment', 'rf_and_link_images', 'rf_and_link_installation_comment', 'electrical_connections_images', 'electrical_installation_comment', 'kplc_solar_images', 'kplc_solar_installation_comment', 'posted_by', 'is_approved', 'created_at', 'updated_at', 'is_active')
+    list_display_links = ('project_name', )
+    list_filter = ('project_name',)
+    search_fields = ('project_name', )
+    list_editable = ('is_active',)
+
+
+admin.site.register(InstallationTeam, InstallationTeamAdmin)
--- erp_construction/models.py
+++ erp_construction/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 
 class User(models.Model):
-    employee_id = models.IntegerField()
+    employee_id = models.CharField(max_length=20)
     password = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -22,3 +23,239 @@ class Project(models.Model):
 
     def __str__(self):
         return self.project_name
+
+
+class FoundationImage(models.Model):
+    project_name = models.ForeignKey(Project, on_delete=models.DO_NOTHING)
+    foundation_and_curing_image = models.ImageField(upload_to='images/CivilWorksTeam/foundation/%Y/%m/%d/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.project_name)
+
+
+class BTSAndGeneatorSlabsImage(models.Model):
+    project_name = models.ForeignKey(Project, on_delete=models.DO_NOTHING)
+    bts_and_generator_slabs_image = models.ImageField(upload_to='images/CivilWorksTeam/slabs/%Y/%m/%d/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.project_name)
+
+
+class SiteWallingImage(models.Model):
+    project_name = models.ForeignKey(Project, on_delete=models.DO_NOTHING)
+    site_walling_image = models.ImageField(upload_to='images/CivilWorksTeam/sitewalling/%Y/%m/%d/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.project_name)
+
+
+class RFAndLinkImage(models.Model):
+    project_name = models.ForeignKey(Project, on_delete=models.DO_NOTHING)
+    rf_and_link_installation_image = models.ImageField(upload_to='images/InstallationTeam/RFAndLink/%Y/%m/%d/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.project_name)
+
+
+class ElectricalImage(models.Model):
+    project_name = models.ForeignKey(Project, on_delete=models.DO_NOTHING)
+    electrical_installation_image = models.ImageField(upload_to='images/InstallationTeam/Electrical/%Y/%m/%d/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.project_name)
+
+
+class KPLCSolarImage(models.Model):
+    project_name = models.ForeignKey(Project, on_delete=models.DO_NOTHING)
+    kplc_solar_installation_image = models.ImageField(upload_to='images/InstallationTeam/KPLCSolar/%Y/%m/%d/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.project_name)
+
+
+class CommercialTeam(models.Model):
+    project_name = models.ForeignKey(Project, on_delete=models.DO_NOTHING)
+    po_file = models.FileField(upload_to='files/CommercialTeam/pofile/%Y/%m/%d/')
+    po_file_comment = models.CharField(max_length=100 , blank=True, null=True)
+    initial_invoice = models.FileField(upload_to='files/CommercialTeam/initialinvoice/%Y/%m/%d/')
+    initial_invoice_comment = models.CharField(max_length=100 , blank=True, null=True)
+    posted_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    is_approved = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.project_name)
+
+
+class ProcurementTeam(models.Model):
+    project_name = models.ForeignKey(Project, on_delete=models.DO_NOTHING)
+    po_steel = models.FileField(upload_to='files/ProcurementTeam/posteel/%Y/%m/%d/')
+    po_steel_comment = models.CharField(max_length=100 , blank=True, null=True)
+    po_electrical_materials = models.FileField(upload_to='files/ProcurementTeam/poelectrical/%Y/%m/%d/')
+    po_electrical_materials_comment = models.CharField(max_length=100 , blank=True, null=True)
+    po_subcontractors = models.FileField(upload_to='files/ProcurementTeam/posubcontractor/%Y/%m/%d/')
+    po_subcontractors_comment = models.CharField(max_length=100 , blank=True, null=True)
+    posted_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    is_approved = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.project_name)
+
+
+class HealthDocumentsCivilTeam(models.Model):
+    project_name = models.ForeignKey(Project, on_delete=models.DO_NOTHING)
+    job_hazard_form = models.FileField(upload_to='files/HealthDocumentsCivilTeam/jobhazard/%Y/%m/%d/')
+    job_hazard_form_comment = models.CharField(max_length=100 , blank=True, null=True)
+    incident_notification_form = models.FileField(upload_to='files/HealthDocumentsCivilTeam/incident/%Y/%m/%d/')
+    incident_notification_form_comment = models.CharField(max_length=100 , blank=True, null=True)
+    toolbox_meeting_form = models.FileField(upload_to='files/HealthDocumentsCivilTeam/toolbox/%Y/%m/%d/')
+    toolbox_meeting_form_comment = models.CharField(max_length=100 , blank=True, null=True)
+    communication_plan_form = models.FileField(upload_to='files/HealthDocumentsCivilTeam/communication/%Y/%m/%d/')
+    communication_plan_form_comment = models.CharField(max_length=100 , blank=True, null=True)
+    posted_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    is_approved = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.project_name)
+
+
+class AccessApprovalCivil(models.Model):
+    project_name = models.ForeignKey(Project, on_delete=models.DO_NOTHING)
+    access_approval = models.FileField(upload_to='files/CivilWorksTeam/accessapproval/%Y/%m/%d/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.project_name)
+
+
+class CivilWorksTeam(models.Model):
+    project_name = models.ForeignKey(Project, on_delete=models.DO_NOTHING)
+    health_documents = models.ManyToManyField(HealthDocumentsCivilTeam)
+    health_documents_comment = ArrayField(models.CharField(max_length=100, blank=True, null=True))
+    access_approvals_field = models.ManyToManyField(AccessApprovalCivil)
+    access_approval_comment = models.CharField(max_length=100 , blank=True, null=True)
+    foundation_and_curing_images = models.ManyToManyField(FoundationImage)
+    foundation_and_curing_comment = models.CharField(max_length=100 , blank=True, null=True)
+    bts_and_generator_slabs_images = models.ManyToManyField(BTSAndGeneatorSlabsImage)
+    bts_and_generator_slabs_comment = models.CharField(max_length=100 , blank=True, null=True)
+    site_walling_images_field = models.ManyToManyField(SiteWallingImage)
+    site_walling_images_comment = models.CharField(max_length=100 , blank=True, null=True)
+    posted_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    is_approved = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.project_name)
+
+    def health_documents_civil(self):
+        return "\n , ".join(str([v.project_name for v in self.health_documents.all()]))
+
+    def foundation_images(self):
+        return "\n , ".join(str([v.project_name for v in self.foundation_and_curing_images.all()]))
+
+    def slabs_images(self):
+        return "\n , ".join(str([v.project_name for v in self.bts_and_generator_slabs_images.all()]))
+
+    def site_walling_images(self):
+        return "\n , ".join(str([v.project_name for v in self.site_walling_images.all()]))
+
+    def access_approvals(self):
+        return "\n , ".join(str([v.project_name for v in self.access_approvals.all()]))
+
+
+class HealthDocumentsInstallationTeam(models.Model):
+    project_name = models.ForeignKey(Project, on_delete=models.DO_NOTHING)
+    job_hazard_form = models.FileField(upload_to='files/HealthDocumentsInstallationTeam/jobhazard/%Y/%m/%d/')
+    job_hazard_form_comment = models.CharField(max_length=100 , blank=True, null=True)
+    incident_notification_form = models.FileField(upload_to='files/HealthDocumentsInstallationTeam/incident/%Y/%m/%d/')
+    incident_notification_form_comment = models.CharField(max_length=100 , blank=True, null=True)
+    toolbox_meeting_form = models.FileField(upload_to='files/HealthDocumentsInstallationTeam/toolbox/%Y/%m/%d/')
+    toolbox_meeting_form_comment = models.CharField(max_length=100 , blank=True, null=True)
+    communication_plan_form = models.FileField(upload_to='files/HealthDocumentsInstallationTeam/communication/%Y/%m/%d/')
+    communication_plan_form_comment = models.CharField(max_length=100 , blank=True, null=True)
+    posted_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    is_approved = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.project_name)
+
+
+class AccessApprovalInstallation(models.Model):
+    project_name = models.ForeignKey(Project, on_delete=models.DO_NOTHING)
+    access_approval = models.FileField(upload_to='files/InstallationTeam/accessapproval/%Y/%m/%d/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.project_name)
+
+
+class InstallationTeam(models.Model):
+    project_name = models.ForeignKey(Project, on_delete=models.DO_NOTHING)
+    health_documents = models.ManyToManyField(HealthDocumentsInstallationTeam)
+    health_documents_comment = ArrayField(models.CharField(max_length=100, blank=True, null=True))
+    access_approvals_field = models.ManyToManyField(AccessApprovalInstallation)
+    access_approval_comment = models.CharField(max_length=100 , blank=True, null=True)
+    rf_and_link_installation_images = models.ManyToManyField(RFAndLinkImage)
+    rf_and_link_installation_comment = models.CharField(max_length=100 , blank=True, null=True)
+    electrical_installation_images = models.ManyToManyField(ElectricalImage)
+    electrical_installation_comment = models.CharField(max_length=100 , blank=True, null=True)
+    kplc_solar_installation_images = models.ManyToManyField(KPLCSolarImage)
+    kplc_solar_installation_comment = models.CharField(max_length=100 , blank=True, null=True)
+    posted_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    is_approved = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.project_name)
+
+    def health_documents_installation(self):
+        return "\n , ".join(str([v.project_name for v in self.health_documents.all()]))
+
+    def rf_and_link_images(self):
+        return "\n , ".join(str([v.project_name for v in self.rf_and_link_installation_images.all()]))
+
+    def electrical_connections_images(self):
+        return "\n , ".join(str([v.project_name for v in self.electrical_installation_images.all()]))
+
+    def kplc_solar_images(self):
+        return "\n , ".join(str([v.project_name for v in self.kplc_solar_installation_images.all()]))
+
+    def access_approvals(self):
+        return "\n , ".join(str([v.project_name for v in self.access_approvals.all()]))
