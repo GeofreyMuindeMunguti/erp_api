2538745d07e090d3d7ad90b99a1ee8cbea45633e
mwerumuchai <mwerumuchahi@gmail.com>
2019-05-08 09:35:57 +0300

    create both Employee and Custom User model

--- erp_construction/models.py
+++ erp_construction/models.py
@@ -6,10 +6,7 @@ from django.conf import settings
 
 from django.utils import timezone
 
-from .managers import CustomUserManager
-
 # Create your models here.
-
 class Employee(models.Model):
     employee_id= models.CharField(max_length=150)
     first_name= models.CharField(max_length=150)
@@ -29,44 +26,18 @@ class Employee(models.Model):
         return employees
 
     @classmethod
-    def get_single_emp(cls, county_id):
+    def get_single_emp(cls, employee_id):
         single_emp = Employee.objects.get(employee=employee_id)
         return single_emp
 
 
-# User.profile = property(lambda u: Employee.objects.get_or_create(user=u)[0])
 
-class User(AbstractUser):
-    username = models.CharField(blank=True, null=True,max_length=100)
-    email = models.EmailField(_('email address'), unique=True,max_length=250)
-
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    employee_id = models.ForeignKey(Employee, null=True, on_delete=models.DO_NOTHING, related_name='employees')
+    username = models.CharField(blank=True, null=True, max_length=150)
+    REQUIRED_FIELDS = ['employee_id', 'username']
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
     def __str__(self):
-        return "{}".format(self.email)
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
-    employee_id= models.ForeignKey(Employee, null=True, on_delete=models.DO_NOTHING, related_name='employees')
-    team= models.CharField(max_length=150)
-    position= models.CharField(max_length=150)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    is_active = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.user.username
-
-    @classmethod
-    def get_profiles(cls):
-        userProfiles = UserProfile.objects.all()
-        return userProfiles
-
-    @classmethod
-    def get_single_profile(cls, user_id):
-        single_profile = UserProfile.objects.get(User=user_id)
-        return single_profile
-
-
-User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
+        return self.employee_id
