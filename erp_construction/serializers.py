a57b2b79eb9368ca0b1c976134b086d4bca9be08
mwerumuchai <mwerumuchahi@gmail.com>
2019-05-12 00:16:19 +0300

    serialize the users list, login

---
+++ erp_construction/serializers.py
@@ -0,0 +1,73 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.models import User
+from . models import *
+
+# UserList
+
+class UserloginSerializer(serializers.ModelSerializer):
+    username = serializers.CharField( max_length=32, validators=[UniqueValidator(queryset=User.objects.all())])
+    password = serializers.CharField(min_length=5, write_only=True)
+
+    def create(self,validated_data):
+        user = User.objects.create_user(validated_data['username'], validated_data['password'])
+        return user
+
+    def update(self, instance, validated_data):
+        profile_data = validated_data.pop('profile')
+        password = validated_data.pop('password')
+        profile = instance.profile
+
+        instance.email = validated_data.get('email', instance.email)
+        instance.save()
+
+        profile.team = profile_data.get('team', profile.team)
+        profile.position = profile_data.get('position', profile.position)
+        profile.save()
+
+        return instance
+
+    class Meta:
+        model = User
+        fields = ('url','username', 'password')
+        read_only_fields = ('is_active', 'created_at', 'updated_at')
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('user_id','team','position')
+        read_only_fields = ('is_active', 'is_staff')
+
+    def create(self, validated_data):
+        profile_data = validated_data.pop('profile')
+        password = validated_data.pop('password')
+        user = UserSerializer.create(UserSerializer(), validated_data=user_data)
+        user.set_password(password_data)
+        user.save()
+        userprofile = User.objects.create(user=user, **validated_data)
+        return userprofiler
+
+    def update(self, instance, validated_data):
+        profile_data = validated_data.pop('profile')
+        password = validated_data.pop('password')
+        profile = instance.profile
+
+        instance.email = validated_data.get('email', instance.email)
+        instance.save()
+
+        profile.team = profile_data.get('team', profile.team)
+        profile.position = profile_data.get('position', profile.position)
+        profile.save()
+
+        return instance
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    A UserProfile serializer to return the UserProfile details
+    """
+    profile = UserProfileSerializer(required=True)
+
+    class Meta:
+        model = User
+        fields = ('url','username', 'email', 'first_name', 'last_name', 'password','profile')
