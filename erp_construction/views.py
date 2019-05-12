2f46d2b33353bf3429d36c66053ff64157e01976
extraordinaire <peter@chenchatech.com>
2019-05-12 20:34:20 +0300

    Permission classes to be added on defaultmixing class

--- erp_construction/views.py
+++ erp_construction/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from .serializers import ProjectSerializer, ProcurementTeamSerializer, HealthDocumentsCivilTeamSerializer, AccessApprovalCivilSerializer, CivilWorksTeamSerializer, FoundationImageSerializer, BTSAndGeneatorSlabsImageSerializer, SiteWallingImageSerializer, CommercialTeamSerializer, SafaricomTeamSerializer
 from rest_framework import generics, permissions, viewsets, serializers, permissions, filters, status
 from .models import Project, ProcurementTeam, HealthDocumentsCivilTeam, AccessApprovalCivil, CivilWorksTeam, FoundationImage, BTSAndGeneatorSlabsImage, SiteWallingImage, CommercialTeam, SafaricomTeam
-# from drf_nested_field_multipart import NestedMultipartParser
-# from rest_framework.parsers import JSONParser
 
 
 class DefaultsMixin(object):
