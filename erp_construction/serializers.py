153fb49cd1537a0c507449daea246bfe866cf61a
mwerumuchai <mwerumuchahi@gmail.com>
2019-05-16 10:13:05 +0300

    remove none authentication code

--- erp_construction/serializers.py
+++ erp_construction/serializers.py
@@ -4,7 +4,7 @@ from rest_framework.validators import UniqueValidator
 
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
-from .models import CustomUser, Project, ProcurementTeam, HealthDocumentsCivilTeam, AccessApprovalCivil, CivilWorksTeam, FoundationImage, BTSAndGeneatorSlabsImage, SiteWallingImage, CommercialTeam, SafaricomTeam
+from .models import *
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
@@ -47,83 +47,3 @@ class UserSerializer(serializers.ModelSerializer):
         profile.save()
 
         return instance
-
-
-class ProjectSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Project
-        fields = ('__all__')
-        read_only_fields = ('created_at', 'updated_at', 'is_active')
-
-
-class ProcurementTeamSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = ProcurementTeam
-        fields = ('__all__')
-        read_only_fields = ('created_at', 'updated_at', 'is_active')
-
-
-class CommercialTeamSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = CommercialTeam
-        fields = ('__all__')
-        read_only_fields = ('created_at', 'updated_at', 'is_active')
-
-
-class HealthDocumentsCivilTeamSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = HealthDocumentsCivilTeam
-        fields = ('__all__')
-        read_only_fields = ('created_at', 'updated_at', 'is_active')
-
-
-class AccessApprovalCivilSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = AccessApprovalCivil
-        fields = ('__all__')
-        read_only_fields = ('created_at', 'updated_at', 'is_active')
-
-
-class FoundationImageSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = FoundationImage
-        fields = ('__all__')
-        read_only_fields = ('created_at', 'updated_at', 'is_active')
-
-
-class BTSAndGeneatorSlabsImageSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = BTSAndGeneatorSlabsImage
-        fields = ('__all__')
-        read_only_fields = ('created_at', 'updated_at', 'is_active')
-
-
-class SiteWallingImageSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = SiteWallingImage
-        fields = ('__all__')
-        read_only_fields = ('created_at', 'updated_at', 'is_active')
-
-
-class CivilWorksTeamSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = CivilWorksTeam
-        fields = ('__all__')
-        read_only_fields = ('created_at', 'updated_at', 'is_active')
-
-
-class SafaricomTeamSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = SafaricomTeam
-        fields = ('__all__')
-        read_only_fields = ('created_at', 'updated_at', 'is_active')
