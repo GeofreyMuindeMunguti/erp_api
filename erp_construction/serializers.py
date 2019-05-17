6d90be29249da9460f72858ad418b78d7f223f29
extraordinaire <peter@chenchatech.com>
2019-05-17 13:34:15 +0300

    Add token and user serilizer so that branch works

--- erp_construction/serializers.py
+++ erp_construction/serializers.py
@@ -1,7 +1,57 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.models import User
-from .models import CustomUser, Project, ProcurementTeam, HealthDocumentsCivilTeam, AccessApprovalCivil, CivilWorksTeam, FoundationImage, BTSAndGeneatorSlabsImage, SiteWallingImage, CommercialTeam, SafaricomTeam
+from .models import CustomUser, Project, ProcurementTeam, HealthDocumentsCivilTeam, AccessApprovalCivil, CivilWorksTeam, FoundationImage, BTSAndGeneatorSlabsImage, SiteWallingImage, CommercialTeam, SafaricomTeam,InstallationTeam,AccessApprovalInstallation,KPLCSolarImage,HealthDocumentsInstallationTeam, RFAndLinkImage,ElectricalImage
+from rest_framework.authtoken.models import Token
+
+
+class TokenSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Token
+        fields = ('key', 'user_id')
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CustomUser
+        fields = ('user_id', 'team', 'position')
+        read_only_fields = ('is_active', 'is_staff')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    A UserProfile serializer to return the UserProfile details
+    """
+    profile = UserProfileSerializer(required=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'password', 'profile')
+
+    def create(self, validated_data):
+        profile_data = validated_data.pop('profile')
+        password = validated_data.pop('password')
+        user = UserSerializer.create(UserSerializer(), validated_data=profile_data)
+        user.set_password(password)
+        user.save()
+        userprofile = User.objects.create(user=user, **validated_data)
+        return userprofile
+
+    def update(self, instance, validated_data):
+        profile_data = validated_data.pop('profile')
+        password = validated_data.pop('password')
+        profile = instance.profile
+
+        instance.email = validated_data.get('email', instance.email)
+        instance.save()
+
+        profile.team = profile_data.get('team', profile.team)
+        profile.position = profile_data.get('position', profile.position)
+        profile.save()
+
+        return instance
 
 
 class ProjectSerializer(serializers.ModelSerializer):
