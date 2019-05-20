0456447e2bf1680cda5cbb5b5185e4d201279b87
mwerumuchai <mwerumuchahi@gmail.com>
2019-05-20 14:51:31 +0300

    Register User app Separate CustomUser from erp_construction app Run Migration

--- erp_construction/models.py
+++ erp_construction/models.py
@@ -1,22 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from users.models import CustomUser
 
 
 # Create your models here.
-class CustomUser(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    team = models.CharField(max_length=150, unique=True)
-    position = models.CharField(max_length=500, blank=False)
-
-    def __str__(self):
-        return self.user.username
-
-    @classmethod
-    def get_employees(cls):
-        employees = CustomUser.objects.all()
-        return employees
-
-    @classmethod
-    def get_single_emp(cls, username):
-        single_emp = CustomUser.objects.get(employee=username)
-        return single_emp
---
+++ erp_mservies/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ErpMserviesConfig(AppConfig):
+    name = 'erp_mservies'
