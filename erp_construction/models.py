000701c936caf59ae06a82c7f00561fcc6d79ba5
extraordinaire <peter@chenchatech.com>
2019-05-29 12:26:45 +0300

    Make neat.

--- erp_construction/models.py
+++ erp_construction/models.py
@@ -6,6 +6,7 @@ from datetime import datetime, timezone
 
 # Create your models here.
 
+
 class Category(models.Model):
     category_name = models.CharField(max_length=100, unique=True)
     created_by = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING)
