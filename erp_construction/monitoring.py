82d79c1a8b4ff9f3bc2486f7dfda8fd58ca666eb
extraordinaire <peter@chenchatech.com>
2019-06-12 17:40:36 +0300

    Get projects status, using FA comment instead of FA itself. Working on it.

--- erp_construction/monitoring.py
+++ erp_construction/monitoring.py
@@ -4,11 +4,13 @@ from rest_framework.response import Response
 
 """ GET TASK STATUS FOR MONITORING"""
 
+"""CURRENTLY CHECKING COMMENT BUT THAT SHOULD BE FILE CHECK"""
+
 
 class TaskStatusView(APIView):
 
     def get(self, request):
         total_projects = Project.objects.all().count()
-        projects_open = Project.objects.filter(final_acceptance_cert__isnull=False).count()
-        projects_closed = Project.objects.filter(final_acceptance_cert__isnull=True).count()
+        projects_open = Project.objects.all().filter(final_acceptance_cert_comment__exact=None).count()
+        projects_closed = Project.objects.all().filter(final_acceptance_cert_comment__isnull=False).count()
         return Response({'total_projects': total_projects, 'open': projects_open, 'closed': projects_closed, })
