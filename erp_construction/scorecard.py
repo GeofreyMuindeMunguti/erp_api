bd41d61174f5be6502cb52a3ff8b12fa21bf1810
extraordinaire <peter@chenchatech.com>
2019-06-16 21:57:11 +0300

    Code never lies, Comments sometimes do.

--- erp_construction/scorecard.py
+++ erp_construction/scorecard.py
@@ -17,8 +17,9 @@ class TurnAroundTimeView(APIView):
             turn_around_time = turn_around_time + time.turn_around_time()
         average_turn_around_time = (turn_around_time/project_count)
         return Response({'average_turn_around_time': average_turn_around_time, })
-        # turn_around_time_data = Project.objects.aggregate(turn_around_time=Avg('project__turn_around_time'))
-        # return Response({'turn_around_time': turn_around_time_data, })
+
+
+"""GET NUMBER OF PURCHASE ORDERS IN THE SYSTEM"""
 
 
 class TotalPurchaseOrdersView(APIView):
@@ -26,6 +27,9 @@ class TotalPurchaseOrdersView(APIView):
     def get(self, request):
         purchase_order_data = ProjectPurchaseOrders.objects.all().count()
         return Response({'total_purchase_orders': purchase_order_data, })
+
+
+"""GET REVENUE FOR AN INDIVISUAL PROJECT USING TOTAL VALUE OF PURCHASE ORDER"""
 
 
 class RevenueDetailView(APIView):
@@ -37,6 +41,9 @@ class RevenueDetailView(APIView):
             return Response({'revenue_per_project': revenue_per_project, })
         except Exception as e:
             return Response({'Error': 'PO for project does not exist'})
+
+
+"""GET REVENUE FOR ALL PROJECTS IN THE SYSTEM USING VALUE OF PURCHASE ORDER"""
 
 
 class RevenueListView(APIView):
