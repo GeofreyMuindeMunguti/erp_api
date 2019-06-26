17e35e5cca59b1a52a093df35acbb578f44cbb1b
gibeongideon <kipngeno.gibeon@gmail.com>
2019-06-26 20:26:16 +0300

    validate if file to be compressed exist before compression

--- erp_construction/fileshandler/compressedfiles.py
+++ erp_construction/fileshandler/compressedfiles.py
@@ -7,6 +7,7 @@ from rest_framework.views import APIView
 from erp_construction.serializers   import ProjectSerializer
 from django.http import Http404
 from django.conf import settings
+import os
 #################################FILES Compression Block#####################################################
 
 
@@ -22,7 +23,6 @@ class CompressedFilesDownload(APIView):
 
     def make_tarfile(self,output_filename, source_dir):
         import tarfile
-        import os
         with tarfile.open(output_filename, "w:xz") as tar:  # w.bz2 w.gz
             tar.add(source_dir, arcname=os.path.basename(source_dir))
             return output_filename
@@ -32,9 +32,29 @@ class CompressedFilesDownload(APIView):
         #TO DO 
         #implement gZIP here instead of  TAR files
 
-        cfile = self.make_tarfile('media/projects/{}/files.tar.xz'.format(self.projectobject.project_name),'media/projects/{}/files/'.format(self.projectobject.project_name))
-        cmage = self.make_tarfile('media/projects/{}/images.tar.xz'.format(self.projectobject.project_name),'media/projects/{}/images/'.format(self.projectobject.project_name))
-    
+               ## Begin BLOCK to validate if files and images has been uploaded yet before compression and download
+                # TO DO # rewrite below code in more  pythonic way : may be write vadidate function
+
+        
+       
+        if os.path.exists(os.path.join('media','projects','{}'.format(self.projectobject),'files')) is False:
+            cfile = 'Files does not exist'
+            if os.path.exists(os.path.join('media','projects','{}'.format(self.projectobject),'images')) is False:
+                cmage = 'Images does not exist'
+            else:
+                cmage = self.make_tarfile('media/projects/{}/images.tar.xz'.format(self.projectobject.project_name),'media/projects/{}/images/'.format(self.projectobject.project_name))
+            
+        else:
+            if os.path.exists(os.path.join('media','projects','{}'.format(self.projectobject),'images')) is False:
+                cfile = self.make_tarfile('media/projects/{}/files.tar.xz'.format(self.projectobject.project_name),'media/projects/{}/files/'.format(self.projectobject.project_name))
+                cmage = 'Images does not exist'
+            else:
+
+                cfile = self.make_tarfile('media/projects/{}/files.tar.xz'.format(self.projectobject.project_name),'media/projects/{}/files/'.format(self.projectobject.project_name))
+                cmage = self.make_tarfile('media/projects/{}/images.tar.xz'.format(self.projectobject.project_name),'media/projects/{}/images/'.format(self.projectobject.project_name))
+
+               ## END  BLOCK to validate if files and images has been uploaded yet before compression and download
+        
         return cfile ,cmage
 
 
@@ -44,8 +64,11 @@ class CompressedFilesDownload(APIView):
         """
         self.projectobject = self.get_object(pk)
         cfile,cmage =self.update_compressed_files()
+        if type(cfile) is str:
+            pass
+            #TO DO :::Resolve this issue //"http://127.0.0.1:8005/Images does not exist"
+
         
-       
         # TO DO : Find a better way to address issues// Can on DEV /Debug=True
         compressed_project_files = 'http://127.0.0.1:8005/{}'.format(cfile)   ##Hard Coded URLs for testing/Development
         compressed_project_images = 'http://127.0.0.1:8005/{}'.format(cmage)   ##Hard Coded URLs for testing/Development
