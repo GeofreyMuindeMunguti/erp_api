83ded7623ec097d145592b02b02346a9a76073c2
extraordinaire <peter@chenchatech.com>
2019-07-09 13:04:01 +0300

    Fix missing files

--- erp_construction/fileshandler/filesserializers.py
+++ erp_construction/fileshandler/filesserializers.py
@@ -15,57 +15,58 @@ class SiteClearingFilesSerializer(serializers.ModelSerializer):
     class Meta:
         model = SetSiteClearingImage
         fields = ('setting_site_clearing_image_1','setting_site_clearing_image_2','setting_site_clearing_image_3',)
-       # read_only_fields = ('created_at', 'updated_at', 'is_active')
+
 
 class TowerBaseImagesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = TowerBaseImage
         fields = ('towerbase_image_1','towerbase_image_1','towerbase_image_1',)
-       # read_only_fields = ('created_at', 'updated_at', 'is_active')
+
 
 class BindingImagesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = BindingImage
         fields = ('binding_image_1','binding_image_2','binding_image_3',)
-       # read_only_fields = ('created_at', 'updated_at', 'is_active')
+
 
 class SteelFixFormworkImagesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = SteelFixFormworkImage
         fields = ('steel_fix_formwork_image_1','steel_fix_formwork_image_2','steel_fix_formwork_image_3',)
-        #read_only_fields = ('created_at', 'updated_at', 'is_active')
+
+
 class ConcretePourImagesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ConcretePourImage
         fields = ('concrete_pour_curing_image_1','concrete_pour_curing_image_2','concrete_pour_curing_image_3',)
-       # read_only_fields = ('created_at', 'updated_at', 'is_active')
+
+
+class ConcreteCuringImagesSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ConcreteCuringPeriodImage
+        fields = ('concrete_pour_curing_period_image_1','concrete_pour_curing_period_image_2','concrete_pour_curing_period_image_3',)
+
 
 #GENERATOR FOUNDATION
+
+
 class ExcavationImagesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ExcavationImage
         fields = ('excavation_image_1','excavation_image_2','excavation_image_3',)
-        #read_only_fields = ('created_at', 'updated_at', 'is_active')
 
-#GENERATOR FOUNDATION
-class ExcavationImagesSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = ExcavationImage
-        fields = ('excavation_image_1','excavation_image_2','excavation_image_3',)
-        #read_only_fields = ('created_at', 'updated_at', 'is_active')
 
 class ConcreteCuringPeriodImagesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = BS241ConcretePourCuringPeriodImage
         fields = ('bs241_concrete_pour_curing_period_image_1','bs241_concrete_pour_curing_period_image_2','bs241_concrete_pour_curing_period_image_3',)
-       # read_only_fields = ('created_at', 'updated_at', 'is_active')
 
 
 #  BOUNDARY WALL
@@ -75,28 +76,27 @@ class FoundFootPourImagesSerializer(serializers.ModelSerializer):
     class Meta:
         model = FoundFootPourImage
         fields = ('foundfootpour_image_1','foundfootpour_image_2','foundfootpour_image_3',)
-      #  read_only_fields = ('created_at', 'updated_at', 'is_active')
+
 
 class BlockworkPanelConstImagesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = BlockworkPanelConstImage
         fields = ('blockwallpanelconst_image_1','blockwallpanelconst_image_2','blockwallpanelconst_image_3',)
-       # read_only_fields = ('created_at', 'updated_at', 'is_active')
+
 
 class GateInstallationImagesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = GateInstallationImage
         fields = ('gateinstallation_image_1','gateinstallation_image_2','gateinstallation_image_3',)
-        #read_only_fields = ('created_at', 'updated_at', 'is_active')
+
 
 class RazorElectricFenceImagesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = RazorElectricFenceImage
         fields = ('razorelectricfance_image_1','razorelectricfance_image_2','razorelectricfance_image_3')
-       # read_only_fields = ('created_at', 'updated_at', 'is_active')
 
 
 #TOWER & ANTENNA_COAXs
@@ -106,28 +106,27 @@ class TowerErectionImagesSerializer(serializers.ModelSerializer):
     class Meta:
         model = TowerErectionImage
         fields = ('tower_erection_image_1','tower_erection_image_2','tower_erection_image_3',)
-        #read_only_fields = ('created_at', 'updated_at', 'is_active')
+
 
 class TowerPaintImagesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = TowerPaintImage
         fields = ('tower_painting_image_1','tower_painting_image_2','tower_painting_image_3',)
-       # read_only_fields = ('created_at', 'updated_at', 'is_active')
+
 
 class CableWaysImagesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CableWaysImage
         fields = ('cable_ways_image_1','cable_ways_image_2','cable_ways_image_3',)
-        #read_only_fields = ('created_at', 'updated_at', 'is_active')
+
 
 class AntennaCoaxInstallImagesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = AntennaCoaxInstallImage
         fields = ('antenna_coax_installation_image_1','antenna_coax_installation_image_2','antenna_coax_installation_image_3',)
-       # read_only_fields = ('created_at', 'updated_at', 'is_active')
 
 #END
 
@@ -136,7 +135,6 @@ class ProjectPurchaseOrdersFileSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProjectPurchaseOrders
         fields = ('po_file',)
-       # read_only_fields = ('created_at', 'updated_at', 'is_active')
 
 
 class ProjectCostingFileSerializer(serializers.ModelSerializer):
@@ -144,7 +142,6 @@ class ProjectCostingFileSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProjectCosting
         fields = ('project_costing_file',)
-       # read_only_fields = ('created_at', 'updated_at', 'is_active')
 
 
 class CommercialTeamFilesSerializer(serializers.ModelSerializer):
@@ -152,30 +149,43 @@ class CommercialTeamFilesSerializer(serializers.ModelSerializer):
     class Meta:
         model = CommercialTeam
         fields = ('approved_quote_file','initial_invoice',)
-       # read_only_fields = ('created_at', 'updated_at', 'is_active')
+
 
 class ProcurementTeamFilesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ProcurementTeam
-        fields = ('po_steel','po_subcontractors',)
-       # read_only_fields = ('created_at', 'updated_at', 'is_active')
-
+        fields = ('po_steel', 'po_subcontractors', 'po_electrical_materials')
 
 
 class HealthDocumentsFilesCivilTeamSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = HealthDocumentsCivilTeam
-        fields = ('job_hazard_form','incident_notification_form','toolbox_meeting_form','communication_plan_form',)
-       # read_only_fields = ('created_at', 'updated_at', 'is_active')
+        fields = ('job_hazard_form', 'incident_notification_form', 'toolbox_meeting_form', 'communication_plan_form', 'safety_picture')
+
 
 class AccessApprovalFileCivilSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = AccessApprovalCivil
         fields = ('access_approval',)
-       # read_only_fields = ('created_at', 'updated_at', 'is_active')
+
+
+class HealthDocumentsFilesInstallationTeamSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = HealthDocumentsInstallationTeam
+        fields = ('job_hazard_form', 'incident_notification_form', 'toolbox_meeting_form', 'communication_plan_form', 'safety_picture')
+        read_only_fields = ('created_at', 'updated_at', 'is_active')
+
+
+class AccessApprovalFileInstallationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = AccessApprovalInstallation
+        fields = ('access_approval',)
+        read_only_fields = ('created_at', 'updated_at', 'is_active')
 
 
 class UndergroundTasksFilesSerializer(serializers.ModelSerializer):
@@ -183,8 +193,6 @@ class UndergroundTasksFilesSerializer(serializers.ModelSerializer):
     class Meta:
         model = UndergroundTasks
         fields = ('Underground_ducting_and_manholes_image_1','Underground_ducting_and_manholes_image_2','Underground_ducting_and_manholes_image_3',)
-        #read_only_fields = ('created_at', 'updated_at', 'is_active')
-
 
 
 class ReticulationAPSinstallationFilesSerializer(serializers.ModelSerializer):
@@ -192,7 +200,6 @@ class ReticulationAPSinstallationFilesSerializer(serializers.ModelSerializer):
     class Meta:
         model = ReticulationAPSinstallation
         fields = ('Electricalreticulation_APSInstallation_image_1','Electricalreticulation_APSInstallation_image_2','Electricalreticulation_APSInstallation_image_3')
-       # read_only_fields = ('created_at', 'updated_at', 'is_active')
 
 
 class ElectricalEarthingImagesSerializer(serializers.ModelSerializer):
@@ -200,7 +207,6 @@ class ElectricalEarthingImagesSerializer(serializers.ModelSerializer):
     class Meta:
         model = ElectricalEarthing
         fields = ('Earthing_connections_and_testing_image_1','Earthing_connections_and_testing_image_2','Earthing_connections_and_testing_image_3',)
-        #read_only_fields = ('created_at', 'updated_at', 'is_active')
 
 
 class GeneratorInstallationImagesSerializer(serializers.ModelSerializer):
@@ -208,22 +214,20 @@ class GeneratorInstallationImagesSerializer(serializers.ModelSerializer):
     class Meta:
         model = GeneratorInstallation
         fields = ('Generator_and_Fuel_Tank_Installation_image_1','Generator_and_Fuel_Tank_Installation_image_2','Generator_and_Fuel_Tank_Installation_image_3','before_fuel_image_1','before_fuel_image_2','after_fuel_image_1','after_fuel_image_2')
-       # read_only_fields = ('created_at', 'updated_at', 'is_active')
+
 
 class KPLCSolarImagesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = KPLCSolarImage
         fields = ('kplc_solar_installation_image_1','kplc_solar_installation_image_2','kplc_solar_installation_image_3',)
-      #  read_only_fields = ('created_at', 'updated_at', 'is_active')
+
 
 class BTSinstallationTaskImagesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = BTSinstallationTask
         fields = ('BTSinstallation_image_1','BTSinstallation_image_2','BTSinstallation_image_3',)
-      #  read_only_fields = ('created_at', 'updated_at', 'is_active')
-
 
 
 class MWInstallationTaskImagesSerializer(serializers.ModelSerializer):
@@ -231,14 +235,21 @@ class MWInstallationTaskImagesSerializer(serializers.ModelSerializer):
     class Meta:
         model = MWInstallationTask
         fields = ('MWinstallation_image_1','MWinstallation_image_2','MWinstallation_image_3',)
-       # read_only_fields = ('created_at', 'updated_at', 'is_active')
+
 
 class InstallationTeamFilesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = InstallationTeam
-        fields = ('as_built','signoff','rfi_document','snag_document','conditional_acceptance_cert',)
-       # read_only_fields = ('created_at', 'updated_at', 'is_active')
+        fields = ('as_built', 'signoff', 'rfi_document', 'snag_document', 'conditional_acceptance_cert',)
+
+
+class IssueImageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Issues
+        fields = ('issue_image', 'issue_sorted_image',)
+
 
 ################### Main Project Serializer################################
 
@@ -257,37 +268,43 @@ class ProjectFilesSerializer(serializers.ModelSerializer):
     setsiteclearingimage = SiteClearingFilesSerializer ( read_only =True)
     towerbaseimage = TowerBaseImagesSerializer(read_only=True)
     bindingimage = BindingImagesSerializer(read_only=True)
+    steelfixformworkimage = SteelFixFormworkImagesSerializer(read_only=True)
+    concretepourimage = ConcretePourImagesSerializer(read_only=True)
+    concretecuringimage = ConcreteCuringImagesSerializer(read_only=True)
 
-    steelfixformworkimage =SteelFixFormworkImagesSerializer(read_only=True)
-    concretepourcuringimage = ConcretePourImagesSerializer(read_only=True)
-    excavationimage =ExcavationImagesSerializer(read_only=True)
+    excavationimage = ExcavationImagesSerializer(read_only=True)
+    concretepourcuringieriodimage = ConcreteCuringPeriodImagesSerializer(read_only=True)
 
-    concretepourcuringieriodimage =ConcreteCuringPeriodImagesSerializer(read_only=True)
-    foundfootpourimage= FoundFootPourImagesSerializer(read_only=True)
-    blockworkpanelconstimage= BlockworkPanelConstImagesSerializer(read_only=True)
+    foundfootpourimage = FoundFootPourImagesSerializer(read_only=True)
+    blockworkpanelconstimage = BlockworkPanelConstImagesSerializer(read_only=True)
+    gateinstallationimage = GateInstallationImagesSerializer(read_only=True)
+    razorelectricfenceimage = RazorElectricFenceImagesSerializer(read_only=True)
 
-    gateinstallationimage= GateInstallationImagesSerializer(read_only=True)
-    razorelectricfenceimage =RazorElectricFenceImagesSerializer(read_only=True)
-    towererectionimage =TowerErectionImagesSerializer(read_only=True)
+    towererectionimage = TowerErectionImagesSerializer(read_only=True)
     towerpaintimage = TowerPaintImagesSerializer(read_only=True)
+    cablewaysimage = CableWaysImagesSerializer(read_only=True)
+    antennacoaxinstallimage = AntennaCoaxInstallImagesSerializer(read_only=True)
 
-    cablewaysimage= CableWaysImagesSerializer(read_only=True)
-    antennacoaxinstallimage =AntennaCoaxInstallImagesSerializer(read_only=True)
+    projectpurchaseorders = ProjectPurchaseOrdersFileSerializer(read_only=True)
+    projectcosting = ProjectCostingFileSerializer(read_only=True)
+    commercialfiles = CommercialTeamFilesSerializer(read_only=True)
+    procurementfiles = ProcurementTeamFilesSerializer(read_only=True)
 
-    projectpurchaseorders =ProjectPurchaseOrdersFileSerializer(read_only=True)
+    healthdocumentscivilteam = HealthDocumentsFilesCivilTeamSerializer(many = True,read_only =True)
+    accessapprovalcivil = AccessApprovalFileCivilSerializer(many = True,read_only=True)
+    healthdocumentsInstallationteam = HealthDocumentsFilesInstallationTeamSerializer(many = True,read_only =True)
+    accessapprovalinstallation = AccessApprovalFileInstallationSerializer(many = True,read_only=True)
 
-    healthdocumentscivilteam =HealthDocumentsFilesCivilTeamSerializer(many = True,read_only =True)
-    accessapprovalcivil=AccessApprovalFileCivilSerializer(many = True,read_only=True)
-
-    undergroundtasks= UndergroundTasksFilesSerializer(read_only=True)
+    undergroundtasks = UndergroundTasksFilesSerializer(read_only=True)
     reticulationapsinstallation = ReticulationAPSinstallationFilesSerializer(read_only=True)
     electricalearthing = ElectricalEarthingImagesSerializer(read_only =True)
     generatorinstallation = GeneratorInstallationImagesSerializer(read_only=True)
-
     kplcsolarimage = KPLCSolarImagesSerializer(read_only=True)
+
     btsinstallationtask = BTSinstallationTaskImagesSerializer(read_only=True)
     mwinstallationtask = MWInstallationTaskImagesSerializer(read_only=True)
     installationteam = InstallationTeamFilesSerializer(read_only =True)
+    issueimages = IssueImageSerializer(read_only=True)
 
 
     class Meta:
--- erp_construction/urls.py
+++ erp_construction/urls.py
@@ -98,38 +98,43 @@ urlpatterns = [
     path('files/<int:pk>/siteclearingimages/', filesviews.SiteClearingFilesView.as_view()),
     path('files/<int:pk>/towerbaseimages/', filesviews.TowerBaseImagesView.as_view()),
     path('files/<int:pk>/bindingimages/', filesviews.BindingImagesView.as_view()),
-
     path('files/<int:pk>/steelfixformworkimages/', filesviews.SteelFixFormworkImagesView.as_view()),
     path('files/<int:pk>/concretepourimages/', filesviews.ConcretePourImagesView.as_view()),
-    path('files/<int:pk>/excavationimages/', filesviews.ExcavationImagesView.as_view()),
+    path('files/<int:pk>/concretecuringimages/', filesviews.ConcreteCuringImagesView.as_view()),
 
+    path('files/<int:pk>/excavationimages/', filesviews.ExcavationImagesView.as_view()),
     path('files/<int:pk>/concretepourcuringperiodimages/', filesviews.ConcreteCuringPeriodImagesView.as_view()),
+
     path('files/<int:pk>/foundfootpourimage/', filesviews.FoundFootPourImageView.as_view()),
     path('files/<int:pk>/blockworkpanelconstimages/', filesviews.BlockworkPanelConstImagesView.as_view()),
-
     path('files/<int:pk>/gateinstallationimages/', filesviews.GateInstallationImagesView.as_view()),
     path('files/<int:pk>/razorelectricfenceimages/', filesviews.RazorElectricFenceImagesView.as_view()),
-    path('files/<int:pk>/towererectionimages/', filesviews.TowerErectionImagesView.as_view()),
 
+    path('files/<int:pk>/towererectionimages/', filesviews.TowerErectionImagesView.as_view()),
     path('files/<int:pk>/towerpaintimages/', filesviews.TowerPaintImagesView.as_view()),
     path('files/<int:pk>/cablewaysimages/', filesviews.CableWaysImagesView.as_view()),
     path('files/<int:pk>/antennacoaxinstallimages/', filesviews.AntennaCoaxInstallImagesView.as_view()),
 
     path('files/<int:pk>/projectpurchaseorders/', filesviews.ProjectPurchaseOrdersView.as_view()),
     path('files/<int:pk>/projectcostingfile/', filesviews.ProjectCostingFileView.as_view()),
-
     path('files/<int:pk>/commercialteamfiles/', filesviews.CommercialTeamFilesView.as_view()),
     path('files/<int:pk>/procurementteamfiles/', filesviews.ProcurementTeamFilesView.as_view()),
-    path('files/<int:pk>/healthdocumentsfilescivilteam/', filesviews.HealthDocumentsFilesCivilTeamView.as_view()),
 
+    path('files/<int:pk>/healthdocumentsfilescivilteam/', filesviews.HealthDocumentsFilesCivilTeamView.as_view()),
     path('files/<int:pk>/accessapprovalfilecivil/', filesviews.AccessApprovalFileCivilView.as_view()),
+    path('files/<int:pk>/healthdocumentsfilesinstallationteam/', filesviews.HealthDocumentsFilesInstallationTeamView.as_view()),
+    path('files/<int:pk>/accessapprovalfileinstallation/', filesviews.AccessApprovalFileInstallationView.as_view()),
+
+    path('files/<int:pk>/undergroundtasks/', filesviews.UndergroundTasksFilesView.as_view()),
     path('files/<int:pk>/reticulationapsinstallationfiles/', filesviews.ReticulationAPSinstallationFilesView.as_view()),
     path('files/<int:pk>/electricalearthingimages/', filesviews.ElectricalEarthingImagesView.as_view()),
-
     path('files/<int:pk>/generatorinstallationimages/', filesviews.GeneratorInstallationImagesView.as_view()),
+    path('files/<int:pk>/kplcsolar/', filesviews.KPLCSolarImagesView.as_view()),
+
     path('files/<int:pk>/btsinstallationtaskimages/', filesviews.BTSinstallationTaskImagesView.as_view()),
     path('files/<int:pk>/mwinstallationtaskimages/', filesviews.MWInstallationTaskImagesView.as_view()),
     path('files/<int:pk>/installationteamfiles/', filesviews.InstallationTeamFilesView.as_view()),
+    path('files/<int:pk>/issueimages/', filesviews.IssueImageView.as_view()),
 
     path('taskstatus/', TaskStatusView.as_view()),
     path('timesheetsummary/', TimesheetSummaryView.as_view()),
