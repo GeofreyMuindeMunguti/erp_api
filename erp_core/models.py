cb42ff57a5a6572e3d7be6432f82a4880f57d1d7
gibeongideon <kipngeno.gibeon@gmail.com>
2019-07-19 20:58:24 +0300

    add commercial absract model

--- erp_core/models.py
+++ erp_core/models.py
@@ -17,9 +17,29 @@ class CreateProject(models.Model):
     class Meta:
         abstract = True
 
-class ProcurementCore(models.Model):
+
+
+class CommercialFiles(models.Model):
       # TODO
-    po = models.CharField(max_length=100)
+    quote = models.FileField(upload_to='files/Quote/quote/%Y/%m/%d/', blank=True, null=True) # ftts & ftth
+    wayleave_application = models.FileField(upload_to='files/WayLeave/wayleave/%Y/%m/%d/', blank=True, null=True) # ftts & ftth
+    wayleave_approval = models.FileField(upload_to='files/WayLeave/wayleave/%Y/%m/%d/', blank=True, null=True) # ftts & ftth
+
+    description =  models.CharField(max_length=100, blank=True, null=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+class ProcurementFiles(models.Model):
+      # TODO
+    bill_of_materials = models.FileField(upload_to='files/BOM/bom/%Y/%m/%d/', blank=True, null=True) # ftts & ftth
+    material_receipt_order = models.FileField(upload_to='files/BRO/bro/%Y/%m/%d/', blank=True, null=True) #ftts
+
     description =  models.CharField(max_length=100, blank=True, null=True)
     
     created_at = models.DateTimeField(auto_now_add=True)
