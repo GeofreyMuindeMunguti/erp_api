e206e780708aa4ae17c318e1a1c972c4e47237fe
gibeongideon <kipngeno.gibeon@gmail.com>
2019-07-19 22:17:21 +0300

    add customized procurement and commercial models

--- commercial/models.py
+++ commercial/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from erp_core.models import CommercialFiles
+from erp_fibre.models import FTTHProject,FTTSProject
 
-# Create your models here.
+          #####  BTS  COMMERCIAL ########
+
+class BTSCommercialTeam(models.Model):
+    ''' All BTS commercial logic here'''
+    #TODO
+    def __init_(self) :
+        return NotImplementedError
+
+
+             #####  FTTS  COMMERCIAL ########
+class FTTSCommercialTeam(CommercialFiles):
+    project_name = models.OneToOneField(FTTSProject, on_delete=models.DO_NOTHING)
+    project_plan = models.FileField(upload_to='files/Project/Projectplan/%Y/%m/%d/', blank=True, null=True) 
+    fiber_requisition = models.FileField(upload_to='files/PO/Poservice/%Y/%m/%d/', blank=True, null=True)
+    po_subcontractors = models.FileField(upload_to='files/PO/PoSubCon/%Y/%m/%d/', blank=True, null=True)
+    fiber_po_client =models.FileField(upload_to='files/PO/PoSubCon/%Y/%m/%d/', blank=True, null=True)
+    quote = models.FileField(upload_to='files/Quote/quote/%Y/%m/%d/', blank=True, null=True) # ftts & ftth
+    approval = models.BooleanField(default=False)
+
+
+
+            #####  FTTS  COMMERCIAL ########
+
+class FTTHCommercialTeam(CommercialFiles):
+    project_name = models.OneToOneField(FTTHProject, on_delete=models.DO_NOTHING)
+    bill_of_qualities = models.FileField(upload_to='files/BOQ/boq/%Y/%m/%d/', blank=True, null=True) #
+    
--- procurement/models.py
+++ procurement/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from erp_core.models import ProcurementFiles
+from erp_fibre.models import FTTHProject,FTTSProject
 
-# Create your models here.
+          #####  BTS  PROCUREMENT ########
+
+class BTSProcurementTeam(models.Model):
+    ''' All BTS Procurement logic here'''
+    #TODO
+    def __init_(self) :
+        return NotImplementedError
+
+
+             #####  FTTS  PROCUREMENT ########
+class FTTSProcurementTeam(ProcurementFiles):
+    project_name = models.OneToOneField(FTTSProject, on_delete=models.DO_NOTHING)
+    material_requisition = models.FileField(upload_to='files/MR/mr/%Y/%m/%d/', blank=True, null=True)
+    perchase_request = models.FileField(upload_to='files/Quote/quote/%Y/%m/%d/', blank=True, null=True) 
+    po_quote_service = models.FileField(upload_to='files/PO/Poservice/%Y/%m/%d/', blank=True, null=True)
+    po_subcontractors = models.FileField(upload_to='files/PO/PoSubCon/%Y/%m/%d/', blank=True, null=True)
+    
+
+
+
+            #####  FTTS  PROCUREMENT ########
+
+class FTTHProcurementTeam(ProcurementFiles):
+    project_name = models.OneToOneField(FTTHProject, on_delete=models.DO_NOTHING)
+    ten_per_cent_invoice = models.FileField(upload_to='files/Invoice/invoice/%Y/%m/%d/', blank=True, null=True) #
+
+    # material_receipt_order = models.FileField(upload_to='files/BRO/bro/%Y/%m/%d/', blank=True, null=True) #ftts
+
+    # description =  models.CharField(max_length=100, blank=True, null=True)
+    
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
+    # is_active = models.BooleanField(default=True)
