f3979bac63ae1930bcfd74807f6fd5397c0f15cc
gibeongideon <43305107+gibeongideon@users.noreply.github.com>
2019-07-24 19:03:42 +0300

    Update urls.py

--- erpProject/urls.py
+++ erpProject/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from erp_construction.urls import router
-#from erp_fiber_ftts.urls import router
 from users.urls import router
 from inventory.urls import router
 from django.conf import settings
@@ -29,7 +28,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', view=ObtainJWTView.as_view(), name='login'),
     path('erp_construction/', include('erp_construction.urls')),
-    #path('erp_fiber_ftts/', include('erp_fiber_ftts.urls')),
     path('', include('users.urls')),
     path('inventory/', include('inventory.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
