4468200eb44d42478ab4e3ee2de893f3fbffaf40
mwerumuchai <mwerumuchahi@gmail.com>
2019-08-13 13:19:45 +0300

    fix serializers error

--- erp_core/views.py
+++ erp_core/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
-from . import models
-from . import serializers
-from rest_framework import viewsets, permissions
+from erp_core.serializers import *
+from rest_framework import generics, permissions, viewsets, serializers, permissions, filters, status
+from .models import *
+from django.db.models import Sum, F
+from django.contrib.auth.models import User
+from datetime import datetime
+
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from datetime import datetime
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser, FormParser, FileUploadParser
+from rest_framework import status, viewsets
+from rest_framework.decorators import parser_classes
+from rest_framework.decorators import detail_route
+
+from erp_core.viewspermissions import *
 
 
-
-class MainSiteViewSet(viewsets.ModelViewSet):
+class MainSiteViewSet(DefaultsMixin,viewsets.ModelViewSet):
     """ViewSet for the MainSite class"""
 
-    queryset = models.MainSite.objects.all()
-    serializer_class = serializers.MainSiteSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    queryset = MainSite.objects.order_by('created_at')
+    serializer_class = MainSiteSerializer
+
+    search_fields = ('site_name', )
+    ordering_fields = ('updated_at', 'site_name', )
