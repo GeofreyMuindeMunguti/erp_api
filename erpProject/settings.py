19bbf5206b19df9c29271d979adaa5f9f0333c60
mwerumuchai <mwerumuchai@gmail.com>
2019-08-29 16:12:23 +0300

    Update settings.py

--- erpProject/settings.py
+++ erpProject/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
 CACHES = {
    'default': {
        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-       'LOCATION': 'http://68.183.79.24:11211',
+       'LOCATION': '127.0.0.1:11211',
    }
 }
 
